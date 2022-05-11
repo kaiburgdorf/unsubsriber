cbd048757350f0c06b5ed586a21ce59429f5af76
Kai Burgdorf <k@iburgdorf.de>
2022-05-11 21:37:41 +0200

    setup maven project

---
+++ src/main/java/de/kaiburgdorf/unsubscriber/App.java
@@ -0,0 +1,13 @@
+package de.kaiburgdorf.unsubscriber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
